1c47c15a97af47ba1952ebf0953acc8aab51a20b
lonelytears <734277565@qq.com>
2022-04-16 22:19:57 +0800

    新增generator

--- src/main/java/org/acacia/serv/App.java
+++ src/main/java/org/acacia/serv/App.java
@@ -2,7 +2,6 @@ package org.acacia.serv;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"org.acacia"})
